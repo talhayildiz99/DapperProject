4534568b7abb214384cfda709d760b8bbdac3656
Talha <99talhayildiz@gmail.com>
2024-06-27 21:50:17 +0300

    Add project files.

---
+++ DapperProject/Dtos/ProductDots/CreateProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperProject.Dtos.ProductDots
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ DapperProject/Dtos/ProductDots/UpdateProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperProject.Dtos.ProductDots
+{
+    public class UpdateProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
